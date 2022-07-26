0a80697f205c6796ceb00924b9c56465f8bda81e
FedeGaay <107083865+FedeGaay@users.noreply.github.com>
2022-07-26 20:51:01 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,16 +1,12 @@
 function redirigir(){
-    return window.confirm("Estas por redirigirte a la pagina oficial, desea continuar?");
+    confirm("Estas por redirigirte a la pagina oficial, desea continuar?");
 }
 
 function estasseguro(){
     result = window.confirm("Seguro quieres eliminar todo?");
-    if (result == true){
-        return null;
-    } else {
-        console.log("Reestablecido")
-    }
 }
 
 function confirmacion(){
     alert("Consulta enviada con exito.")
 }
+
